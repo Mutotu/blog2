3cd44416276f379ee08248e07d0a4b790d7d1587
mutotu <muttalip.aydemir@gmail.com>
2022-10-06 14:02:41 -0700

    added jsonserver

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://6634-2601-646-203-5580-00-3c4f.ngrok.io",
+  baseURL: "http://0b45-2601-646-203-5580-00-3c4f.ngrok.io",
 });
